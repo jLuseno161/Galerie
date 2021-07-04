c51614d15c611e82c859ad53687f1cffd59adb34
jLuseno161 <joyluseno2@gmail.com>
2021-07-04 11:01:48 +0300

    function to search image by category

--- la_galerie/views.py
+++ la_galerie/views.py
@@ -9,3 +9,13 @@ def showpage(request):
     location=Location.objects.all()
 
     return render(request,'galeria.html',{"images":images,"location":location})
+def search_category(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        category = request.GET.get("category")
+        search = Image.search_by_category(category)
+        message = f"{category}"
+        return render(request, 'search.html',{"message":message,"category": search})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
