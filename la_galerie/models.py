0aa22913c25c880ef02e8e5e5415671d6b8a115c
jLuseno161 <joyluseno2@gmail.com>
2021-07-04 11:04:59 +0300

    create model for category

--- la_galerie/models.py
+++ la_galerie/models.py
@@ -10,4 +10,14 @@ class Location(models.Model):
     def get_locations(cls):
         location = Location.objects.all()
         return location
-    
+        
+class Category(models.Model):
+    cat_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.cat_name
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()    
