798584b2ad1ad6ba6ed045ff79be6903181c4c03
jLuseno161 <joyluseno2@gmail.com>
2021-07-04 12:59:41 +0300

    tests for location  model class

--- la_galerie/tests.py
+++ la_galerie/tests.py
@@ -19,3 +19,26 @@ class CategoryTestClass(TestCase):
         category = Category.objects.all()
         self.assertTrue(len(category) <= 0)
 
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(location_name='Nairobi')
+        self.location.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_location(self):
+        self.location.save_location()
+        locat = Location.objects.all()
+        self.assertTrue(len(locat) > 0) 
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        category = Location.objects.all()
+        self.assertTrue(len(category) <= 0)
+    
+    def test_get_location(self):
+        location = Location.get_locations()
+        self.assertTrue(location)
+
+    
