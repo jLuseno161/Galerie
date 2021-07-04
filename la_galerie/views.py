15801678ddaf8ed7c52087ab1733a7a383283186
jLuseno161 <joyluseno2@gmail.com>
2021-07-04 11:03:33 +0300

    save changes

--- la_galerie/views.py
+++ la_galerie/views.py
@@ -9,6 +9,7 @@ def showpage(request):
     location=Location.objects.all()
 
     return render(request,'galeria.html',{"images":images,"location":location})
+
 def search_category(request):
 
     if 'category' in request.GET and request.GET["category"]:
@@ -19,13 +20,16 @@ def search_category(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'search.html',{"message":message})
+
 def image_properties(request,image_id):
 
     image = Image.get_image_by_id(image_id)
 
     return render(request, {"image" : image})
+
 def image_location(request,location_name):
     
     image= Image.fetch_by_location(location_name)
 
     return render(request,'img_location.html',{"image":image})
+
