86ddd5b7c74c127c9e19268499ab2d263d4587e9
jLuseno161 <joyluseno2@gmail.com>
2021-07-04 10:59:01 +0300

    edit url fetch image by location

--- la_galerie/urls.py
+++ la_galerie/urls.py
@@ -8,5 +8,6 @@ urlpatterns=[
         url(r'^$',views.showpage,name='galeria'),
         url(r'^search/', views.search_category, name='search_category'),
         url('^image/(?P<image_id>\d+)/$',views.image_properties, name='image'),
+        url(r'^location/(?P<location_name>\w+)',views.image_location,name = 'location'),
 
 ]
