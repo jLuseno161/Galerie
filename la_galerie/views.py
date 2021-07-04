0ac9da7e0424d05c84e8ef0fa1f337def5262fa3
jLuseno161 <joyluseno2@gmail.com>
2021-07-04 19:04:11 +0300

    fetch location everytime a new page is loaded

--- la_galerie/views.py
+++ la_galerie/views.py
@@ -10,19 +10,25 @@ def showpage(request):
     return render(request,'galeria.html',{"images":images,"location":location})
 def search_category(request):
 
+    location=Location.get_locations()
+
     if 'category' in request.GET and request.GET["category"]:
         category = request.GET.get("category")
         search = Image.search_by_category(category)
         message = f"{category}"
-        return render(request, 'search.html',{"message":message,"category": search})
+        return render(request, 'search.html',{"message":message,"category": search,"location":location})
     else:
-        message = "You haven't searched for any term"
-        return render(request, 'search.html',{"message":message})
+        return render(request, 'search.html')
 
 def image_properties(request,image_id):
+    location=Location.get_locations()
+
     image = Image.get_image_by_id(image_id)
-    return render(request, {"image" : image})
+    return render(request, {"image" : image,"location":location})
 
 def image_location(request,location_name):
+    location=Location.get_locations()
     image= Image.fetch_by_location(location_name)
-    return render(request,'img_location.html',{"image":image})
+    message = f"{location_name}"
+    return render(request, 'img_location.html',{"message":message,"image": image,"location":location})
+   
