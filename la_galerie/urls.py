640af37809fe7a9d88fc32c5e3600a1c8bff3d05
jLuseno161 <joyluseno2@gmail.com>
2021-07-04 10:59:32 +0300

    display static files

--- la_galerie/urls.py
+++ la_galerie/urls.py
@@ -11,3 +11,5 @@ urlpatterns=[
         url(r'^location/(?P<location_name>\w+)',views.image_location,name = 'location'),
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
