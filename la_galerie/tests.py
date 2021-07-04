bd97dc623d9690d6666c9006552ab6e6dd04d24e
jLuseno161 <joyluseno2@gmail.com>
2021-07-04 12:42:09 +0300

    test for category model class

--- la_galerie/tests.py
+++ la_galerie/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Image, Location, Category
 
-# Create your tests here.
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.category = Category(cat_name='Nature')
+        self.category.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save_category(self):
+        self.category.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0) 
