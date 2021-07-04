f7bbcd652c123b8b587170fdfdc9c444c3503f90
jLuseno161 <joyluseno2@gmail.com>
2021-07-04 10:58:32 +0300

    edit url fetch image by id

--- la_galerie/urls.py
+++ la_galerie/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns=[
         url(r'^$',views.showpage,name='galeria'),
         url(r'^search/', views.search_category, name='search_category'),
+        url('^image/(?P<image_id>\d+)/$',views.image_properties, name='image'),
 
 ]
