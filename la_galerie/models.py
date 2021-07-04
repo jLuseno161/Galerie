1fc6e6cbe496f91ca5e2e0afa6f0c80f98850d4e
jLuseno161 <joyluseno2@gmail.com>
2021-07-04 11:05:25 +0300

    create model for Images

--- la_galerie/models.py
+++ la_galerie/models.py
@@ -10,7 +10,7 @@ class Location(models.Model):
     def get_locations(cls):
         location = Location.objects.all()
         return location
-        
+
 class Category(models.Model):
     cat_name = models.CharField(max_length=100)
 
@@ -21,3 +21,20 @@ class Category(models.Model):
 
     def delete_category(self):
         self.delete()    
+
+class Image(models.Model):
+
+    image_name =models.CharField(max_length=200)
+    description =models.TextField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,default='0')
+    location = models.ForeignKey(Location ,on_delete=models.CASCADE,default='0')
+    image=models.ImageField(upload_to='img/')
+    
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
